b724a6c394b8549fdd018923ea7e5c7a6a61461b
mmwerner92 <mmwerner92@gmail.com>
2018-09-14 13:15:29 -0500

    updated permissions on deployment files

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/www/project/
-source /home/ec2-user/www/project-venv/bin/activate
+#source /home/ec2-user/www/project-venv/bin/activate
 ./manage.py makemigrations
 ./manage.py migrate auth
 ./manage.py migrate
