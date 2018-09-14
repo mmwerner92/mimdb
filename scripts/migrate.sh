97c6db6d4ac763cc1b3022ec7789f281157ca582
mmwerner92 <mmwerner92@gmail.com>
2018-09-14 13:21:34 -0500

    Adjusted permissions

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+chown ec2-user:ec2-user /home/ec2-user/www/project -R
 cd /home/ec2-user/www/project/
 #source /home/ec2-user/www/project-venv/bin/activate
 ./manage.py makemigrations
